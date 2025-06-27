9f66b3b62e25ae0548fe6487f93ab972989499b5
RicardoCCossa <ricardoccossa@gmail.com>
2025-06-27 17:38:26 +0200

    Tela de login e cadastro

---
+++ usuarios/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+def login(request):
+    return render(request, "usuario/login.html")
+
+def cadastro(request):
+    return render(request, "usuario/cadastro.html")
